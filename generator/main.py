1db582d2a321fb091a5173fc85c149e986c01b89
Arthur Drozdov <arthur@drozdov.net>
2021-05-04 17:24:34 +0100

    html diff

--- generator/main.py
+++ generator/main.py
@@ -1,5 +1,7 @@
 import os
+from glob import glob
 import logging
+from datetime import datetime
 import difflib as dl
 from generate_learning_task import generate_learning_task
 from run_learning_task import run_task
@@ -9,20 +11,30 @@ from preprocess import get_requests_from_logs
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
+def clear_dir(path):
+    for f in glob(f'{path}/*'):
+        os.remove(f)
+
 if __name__ == '__main__':
     data = os.getenv('DATA', 'synheart-controller-opa-istio.log')
     data_base = data.rsplit('.', 1)[0]
 
     data_dir = os.getenv('DATA_DIR', '../data')
     tasks_dir = os.getenv('TASKS_DIR', '../tasks')
+    clear_dir(tasks_dir)
     models_dir = os.getenv('MODELS_DIR', '../models')
+    clear_dir(models_dir)
     policies_dir = os.getenv('POLICIES_DIR', '../policies')
+    clear_dir(policies_dir)
     diffs_dir = os.getenv('DIFFS_DIR', '../diffs')
+    clear_dir(diffs_dir)
 
-    max_attributes = 10
+    max_attributes = 20
     window_size = 300
 
+    differ = dl.HtmlDiff(tabsize=16)
     all_requests = get_requests_from_logs(f'{data_dir}/{data}')
+    prev_time = None
     for i, w in enumerate(range(0, len(all_requests), window_size)):
         log.info(f'Processing window {i}/{len(all_requests)//window_size}...')
         requests = all_requests[w:w+window_size]
@@ -31,8 +43,7 @@ if __name__ == '__main__':
         with open(task_path, 'w') as f:
             f.write(task)
         model, rule_confidences = run_task(task_path, body_cost)
-        model_path = f'{models_dir}/{data_base}{i}.lp'
-        with open(model_path, 'w') as f:
+        with open(f'{models_dir}/{data_base}{i}.lp', 'w') as f:
             f.write(model)
         new_policy = generate_rego_policy(rule_confidences, data_base)
         with open(f'{policies_dir}/{data_base}{i}.rego', 'w') as f:
@@ -40,5 +51,9 @@ if __name__ == '__main__':
         if i > 0:
             with open(f'{policies_dir}/{data_base}{i-1}.rego', 'r') as f:
                 prev_policy = f.readlines()
-            with open(f'{diffs_dir}/{data_base}{i-1}_{i}.diff', 'w') as f:
-                f.write('\n'.join([diff for diff in dl.unified_diff(prev_policy, new_policy.splitlines(True))]))
+            with open(f'{diffs_dir}/{data_base}{i-1}_{i}.html', 'w') as f:
+                now = datetime.now().strftime("%d/%m/%Y at %H:%M:%S")
+                now_time = f'{data_base} on {now}'
+                f.write(differ.make_file(prev_policy, new_policy.splitlines(True),
+                                         fromdesc=prev_time, todesc=now_time))
+                prev_time = now_time
