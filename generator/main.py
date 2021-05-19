c99dadd89562089c922cf99c53e5af0e97b394a5
Arthur Drozdov <arthur@drozdov.net>
2021-05-19 18:37:05 +0100

    tweak config to minimise denies, adjust relearn schedule

--- generator/main.py
+++ generator/main.py
@@ -48,11 +48,11 @@ if __name__ == '__main__':
     decay = float(config['settings']['decay'])
     drop_threshold = float(config['settings']['drop_threshold'])
     warm_up = int(config['settings']['warm_up'])
-    calibrate_interval = int(config['settings']['calibrate_interval'])
+    calibrate_interval = max(int(config['settings']['calibrate_interval']), 1)
     differ = dl.HtmlDiff(wrapcolumn=80)
 
     all_requests = get_requests_from_logs(f'{data_dir}/{data}')
-    all_requests = all_requests[(len(all_requests) // 2) + 5000:]
+    # all_requests = all_requests[(len(all_requests) // 2) + 5000:]
     log.info(f'Total requests: {len(all_requests)}')
 
     next_set = []
@@ -72,11 +72,10 @@ if __name__ == '__main__':
         deepcopy(learned_requests), learned_distances, max_attributes,
         generalisation, f'{data_base}_1', tasks_dir, models_dir, policies_dir, data_base
     )
-    p_i, t_i = 2, 0
+    p_i, c_i = 2, 0
     w_i += 1
     window = all_requests[(w_i-1) * window_size:w_i * window_size]
     cooldown = 0
-    prev_avg_distances = []
     relearn_high, relearn_low = False, False
     while window:
         cooldown = max(0, cooldown - 1)
@@ -92,14 +91,14 @@ if __name__ == '__main__':
         curr_avg_distances = list(zip(*avg_distances[-len(hd_distances):]))[1]
         mean_avg_d = np.mean(curr_avg_distances)
         std_avg_d = np.std(curr_avg_distances)
-        high_thresh = mean_avg_d + 2 * std_avg_d + mean_avg_d * 0.1 * (1 - t_i / (calibrate_interval + 0.000001))
+        high_thresh = mean_avg_d + 2 * std_avg_d
         low_thresh = max(mean_avg_d - 2 * std_avg_d, 0)
         thresholds.append((w_i, high_thresh, low_thresh))
-        t_i += 1
+        c_i += 1
         log.info(f'Window {w_i:3d} - Avg max distance: {avg_distance:.4f}, '
                  f'learned_size: {len(learned_requests)}, next_size: {sum(map(len, next_set)):4d}, '
                  f'high threshold: {high_thresh:.4f}, low threshold: {low_thresh:.4f}, denies: {num_denies}')
-        if avg_distance > high_thresh and not relearn_high and not relearn_low and cooldown == 0:
+        if avg_distance > high_thresh and not relearn_high and not relearn_low:
             log.info(f'Schedule relearn of policy, as {avg_distance:.4f} > {high_thresh:.4f}')
             relearn_high = True
             relearn_schedule_ws.append((w_i, avg_distance))
@@ -107,9 +106,9 @@ if __name__ == '__main__':
             log.info(f'Schedule relearn of policy, as {avg_distance:.4f} < {low_thresh:.4f}')
             relearn_low = True
             relearn_schedule_ws.append((w_i, avg_distance))
-        elif (relearn_high and avg_distance <= np.mean(prev_avg_distances)
-              or relearn_low and avg_distance >= np.mean(prev_avg_distances)):
-            log.info(f'Relearn {"high" if relearn_high else "low"}')
+        elif (relearn_high and avg_distance <= np.mean(curr_avg_distances)
+              or relearn_low and avg_distance >= np.mean(curr_avg_distances)) or c_i % calibrate_interval == 0:
+            log.info(f'Relearn {"high" if relearn_high else "low" if relearn_low else "calibrate"}')
             next_requests, next_distances = list(zip(*list(reduce(lambda a, b: a + b, next_set))))
             new_policy_path, new_policy_time, new_package = generate_policy(
                 deepcopy(next_requests), next_distances, max_attributes,
@@ -122,11 +121,8 @@ if __name__ == '__main__':
             learned_requests = next_requests
             cooldown = len(hd_distances)
             relearn_high, relearn_low = False, False
-            prev_avg_distances.clear()
             p_i += 1
-            t_i = 0
-        if relearn_high or relearn_low:
-            prev_avg_distances.append(avg_distance)
+            c_i = 0
         w_i += 1
         window = all_requests[(w_i-1) * window_size:w_i * window_size]
 
@@ -143,7 +139,7 @@ if __name__ == '__main__':
         plt.plot(x_relearn, y1_relearn, 'ro', label='relearn')
     # plt.plot([2, 84], [0.23, 2], 'gs', label='new behaviour')
     # plt.hlines(relearn_threshold, x[0], x[-1], linestyles='dashed', label='relearn threshold', colors=['black'])
-    plt.legend(loc='lower right')
+    plt.legend(loc='upper right')
     plt.title('Average max distance of incoming requests to the learning set')
     plt.xlabel(f'Window ({window_size} requests)')
     plt.ylabel(f'Average max distance (approx over the last {len(hd_distances)} windows)')
